49b95862c473590761b3094bd3ab73969e21e190
sunowsir <sunwosir@protonmail.com>
2018-08-27 13:32:16 +0800

    change some scripts

--- Mem_info.sh
+++ Mem_info.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 nowdate=`date +"%Y-%m-%d_%H:%M:%S"`
-total=`free -m | grep "^Mem" | awk '{printf ("%s:%s", $2, $3)}'`
-used=`echo $total | cut -d ":" -f2`
-total=`echo $total | cut -d ":" -f1`
-((surplus=$total-$used))
-proportion=`echo "$used $total" | awk '{printf("%.1f%%", $1/$2*100)}'`
-dynamic_proportion=`echo "$1 $proportion" | awk '{printf("%.1f%%", 0.3*$1+0.7*$2)}'`
 
-echo "${nowdate} ${total}M ${surplus}M ${proportion} ${dynamic_proportion}"
+mem_info=`free -m | grep "^Mem" | awk -v last_per=$1 '{printf("%sM %sM %.1f%% %.1f%%", $2, $7, $3 / $2 * 100, 0.3 * last_per + 0.7 * ($3 / $2 * 100));}'`
 
+echo "$nowdate $mem_info"
