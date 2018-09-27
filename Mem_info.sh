16a80efd167299a136206bfdb5441fb5d89f5931
sunowsir <sunowsir@protonmail.com>
2018-09-27 12:21:47 +0800

    optimization Mem_info.sh

--- Mem_info.sh
+++ Mem_info.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-nowdate=`date +"%Y-%m-%d_%H:%M:%S"`
+free -m | grep "^Mem" | awk -v last_per=$1 -v nowdate=$(date +"%Y-%m-%d_%H:%M:%S") '
+{
+    printf("%s %sM %sM %.1f%% %.1f%%\n", nowdate, $2, $7, $3 / $2 * 100, 0.3 * last_per + 0.7 * ($3 / $2 * 100));
+}'
 
-mem_info=`free -m | grep "^Mem" | awk -v last_per=$1 '{printf("%sM %sM %.1f%% %.1f%%", $2, $7, $3 / $2 * 100, 0.3 * last_per + 0.7 * ($3 / $2 * 100));}'`
-
-echo "$nowdate $mem_info"
