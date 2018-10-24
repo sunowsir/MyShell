f26eb3f69fc6e895f43240b751503747f7e9b61e
sunowsir <sunowsir@protonmail.com>
2018-10-24 20:13:06 +0800

    debug : re_pic.sh

--- re_pic.sh
+++ re_pic.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#########################################################OTHER FUNCTIONS START
+
+# Help information. # 
 Usage() {
     echo "You need install wget , curl and change mode before use the script. "
     echo "Install wget(Debain) : sudo apt-get install wget"
@@ -21,156 +24,273 @@ Usage() {
     exit 0
 }
 
-if [[ $1 == '--help' ]]; then 
-    Usage
-fi
+# Determine if the image exists, if there is a size of zero. # 
+function Judge_img() {
+    # ${1} : ${save_path} 
+    # ${2} : ${img_name}
 
-suffix='**'
-para_num=0
-search_name='**'
-
-for arg in $@;
-do
-    para[${para_num}]=${arg}
-    ((para_num++))
-done
-
-for ((i = 0; i < para_num; i++));
-do
-    case ${para[${i}]} in
-        "-u" | "--url" | "--URL" | "--Url")
-            ((i++))
-            source_url=${para[${i}]}
-        ;;
-        "-p"| "--path" | "--Path" | "--PATH")
-            ((i++))
-            save_path=${para[${i}]}
-        ;;
-        "-s" | "--suffix" | "--Suffix" | "--SUFFIX")
-            ((i++))
-            suffix=${para[${i}]}
-        ;;
-        "-n" | "--name" | "--Name" | "--NAME")
-            ((i++))
-            search_name=${para[${i}]}
-        ;;
-        "-w" | "--word" | "--Word" | "--WORD")
-            ((i++))
-            add_word=${para[${i}]}
-        ;;
-        *)
-            Usage
-        ;;
-    esac
-done
-
-if [[ "x${save_path}" == "x" ]];then 
-    touch re_pic.d
-    save_path="./re_pic.d"
-fi
-
-# Script dirrctory.
-nowpath=$(cd $(dirname "$0") && pwd)
-
-# Get the URL protocol.
-source_protocol=$(echo "${source_url}" | grep -Eo 'http\w*:')
-
-# Jump to the directory where the script is located.
-cd ${nowpath}
-
-# if don't hava the file ,touch the file.
-if [[ ! -f ./.re_pic.get.webcode ]];then
-    touch ./.re_pic.get.webcode
-fi
-
-# Get the web page code and save in .re_pic.get.webcode.
-curl --retry 3 --connect-timeout 15 ${source_url} -L -o ./.re_pic.get.webcode --silent
-
-wait
-
-# Get download all images url.
-urls=$(cat ./.re_pic.get.webcode | grep -Eo '<img\s*[^>]*' | tr -s '"' '\n' | tr -s "'" "\n" | grep -Eo '(\w*:)*(\/)*(\/\S+)+' | grep "${suffix}" | sort -u)
-# Download images.
-
-img_nums=0
-
-echo
-echo -e "\033[1;33mSource : \033[1;34m${source_url}\033[0m"
-
-# Print informathon of get web source code failed.
-if [[ "x$(cat ./.re_pic.get.webcode 2> /dev/null)" == "x" ]];then 
-    echo -e "\033[1;31m$(date +"%Y-%m-%d %H:%M:%S") Failed to get page source code(connection timeout within fifteen seconds).\033[0m"
-fi
-
-for img_url in `echo "${urls}" | tr -s " " "\n"`
-do
-
-    # Get the image name.
-    img_name=$(echo "${img_url}" | grep -Eo '\S*\.\w\w\w' | xargs -I {} basename {})
-
-    if [[ "x$(echo "${img_name}" | grep "${search_name}")" == "x" ]];then
-        continue;
+    # Determine if the image is duplicated. # 
+    if [[ "$(ls ${1}${2} 2> /dev/null | grep -w ${2})" == "${2}" ]];then
+        return 2
+    # Determine if the image size is zero. # 
+    elif [[ $(ls -al ${1}${2} 2> /dev/null | awk '{print $5}') -eq 0 ]];then 
+        return 1
+    else 
+        return 0
     fi
+}
 
-    # If there is no agreement in picture URL, add the agreement for hte URL's web address.
-    if [[ "x$(echo "${img_url}" | grep -Eo 'http\S*')" == "x" ]];then
-        img_url=$(echo "${img_url}" | sed 's/^\/*//g')
-        # Add the user specified missing path to picture URL.
-        if [[ "x${add_word}" != "x" ]];then
-            img_url="${source_protocol}//${add_word}${img_url}"
-        else 
-            img_url="${source_protocol}//${img_url}"
-        fi
-    fi
+# Download. # 
+function Down_img() {
+    # ${1} : ${save_path} 
+    # ${2} : ${img_name}
+    # ${3} : ${img_url}
 
-    # If there is no suffix in the picture name obtained from picture URL, add the ".jpg"  suffix to the name of the picture.
-    if [[ "x$(echo "${img_name}" | cut -d '.' -f 2)" == "x" ]];then
-        img_name="${img_name}.jpg"
-    fi
-
-    # add color for print information.
-    echo -e -n "\033[1;32m"
-
-    if [[ "x$(echo "${save_path}" | grep -Eo '\/$')" == "x" ]];then
-        save_path="${save_path}/"
-    fi
+    wget -c -nv -nc --dns-timeout=5 --read-timeout=6 --connect-timeout=15 --output-document=${1}${2} ${3} > /dev/null 2>&1
+    wait
     
-    path_name=${save_path}${img_name}
-    # Skip repeating pictures.
-    if [[ $(ls ${save_path} | grep -w ${img_name}) == ${img_name} ]];then
-        nowtime=$(date +"%Y-%m-%d %H:%M:%S")
-        echo -e "\033[1;31m${nowtime} The file already exists : ${path_name:(-30)}\033[0m"
-        continue
-    fi
-
-    # echo "url : ${img_url}"
-    # echo "path : ${save_path}${img_name}"
-    # Download image from ${img_url}
-    wget -c -nv -nc --tries=2 --output-document=${save_path}${img_name} ${img_url} > /dev/null 2>&1
+    # Try again, if this image download failed. # 
+    try_num=5
+    while [[ ${try_num} ]];
+    do
+        Judge_img ${1} ${2}
+        if [[ ${?} -ne 0 ]];then 
+            rm -rf ${1}${2}
+            wget -c -nv -nc --dns-timeout=5 --read-timeout=6 --connect-timeout=10 --output-document=${1}${2} ${3} > /dev/null 2>&1
+            wait
+        else 
+            break
+        fi
+        ((try_num--))
+    done
 
     # curl --retry 3 --connect-timeout 15 -# -o ${save_path}${img_name} ${img_url}
 
     wait
-    echo "$(date +"%Y-%m-%d %H:%M:%S") ${img_url:0:30}... -> ...${path_name:(-30)}"
+
+    Judge_img ${1} ${2}
+    if [[ ${?} -ne 0 ]];then 
+        return 1
+    else 
+        return 0
+    fi
+}
+
+function Para_analysis() {
+    suffix='**'
+    para_num=0
+    search_name='**'
     
-    echo -e -n "\033[0m"
-    ((img_nums++))
+    # Save all parameters to the array 'para[]'.  # 
+    for arg in $@;
+    do
+        para[${para_num}]=${arg}
+        ((para_num++))
+    done
+    
+    for ((i = 0; i < para_num; i++));
+    do
+        case ${para[${i}]} in
+            "-u" | "--url" | "--URL" | "--Url")
+                ((i++))
+                source_url=${para[${i}]}
+            ;;
+            "-p"| "--path" | "--Path" | "--PATH")
+                ((i++))
+                save_path=${para[${i}]}
+                if [[ "x$(echo "${save_path}" | grep -Eo '\/$')" == "x" ]];then
+                    save_path="${save_path}/"
+                fi
+            ;;
+            "-s" | "--suffix" | "--Suffix" | "--SUFFIX")
+                ((i++))
+                suffix=${para[${i}]}
+            ;;
+            "-n" | "--name" | "--Name" | "--NAME")
+                ((i++))
+                search_name=${para[${i}]}
+            ;;
+            "-w" | "--word" | "--Word" | "--WORD")
+                ((i++))
+                add_word=${para[${i}]}
+            ;;
+            *)
+                Usage
+            ;;
+        esac
+    done
+    
+}
 
-done
+function Get_code() {
+    # ${1} : ${source_url}
+    
+    # Get the web page code and save in .re_pic.get.webcode. # 
+    curl --connect-timeout 10 ${1} -L -o ./.re_pic.get.webcode --silent
+    wait
 
+    try_num=5
+    while [[ $((try_num--)) ]];
+    do
+        if [[ "x$(cat ./.re_pic.get.webcode 2> /dev/null)" == "x" ]];then 
+            curl --connect-timeout 10 ${1} -L -o ./.re_pic.get.webcode --silent
+            wait
+        else 
+            break
+        fi
+        
+    done
+    wait
+    
+    # Print informathon of get web source code failed. # 
+    if [[ "x$(cat ./.re_pic.get.webcode 2> /dev/null)" == "x" ]];then 
+        return 1
+    else 
+        return 0
+    fi
+}
+
+function Get_urls() {
+    # ${1} : ${suffix}
+
+    # Get download all images url. # 
+    if [[ "x${suffix}" == "x" ]];then
+        urls=$(cat ./.re_pic.get.webcode | grep -Eo '<img\s*[^>]*' | tr -s '"' '\n' | tr -s "'" "\n" | grep -Eo '(\w*:)*(\/)*(\/\S+)+' | sort -u)
+    else 
+        urls=$(cat ./.re_pic.get.webcode | grep -Eo '<img\s*[^>]*' | tr -s '"' '\n' | tr -s "'" "\n" | grep -Eo '(\w*:)*(\/)*(\/\S+)+' | grep "${1}" | sort -u)
+    fi
+    
+    if [[ "x${urls}" == "x" ]];then
+        return 1
+    else
+        return 0
+    fi
+}
+
+#########################################################OTHER FUNCTIONS END
+
+function main() {
+    
+    if [[ $1 == '--help' ]]; then 
+        Usage
+    fi
+    
+    suffix='**'
+    para_num=0
+    search_name='**'
+    # Script directory.
+    nowpath=$(cd $(dirname "$0") && pwd)
+    cd ${nowpath}
+    
+    # Parameter analysis # 
+    Para_analysis $@
+    
+    if [[ "x${source_url}" == "x" ]];then
+        echo -e "\033[1;31m URL is empty."
+        Usage
+    else
+        echo
+        echo -e "\033[1;32mSource : \033[1;34m${source_url}\033[0m"
+
+        # Get the URL protocol. # 
+        source_protocol=$(echo "${source_url}" | grep -Eo 'http\w*:')
+    fi
+    
+    if [[ "x${save_path}" == "x" ]];then 
+        touch re_pic.d
+        save_path="./re_pic.d"
+    fi
+    
+    # if don't hava the file ,touch the file. # 
+    if [[ ! -f ./.re_pic.get.webcode ]];then
+        touch ./.re_pic.get.webcode
+    fi
+    
+    # Get the web code.  # 
+    Get_code ${source_url}
+    if [[ ${?} -ne 0 ]];then
+        echo -e "\033[1;31mERROR\033[0m : Failed to get page source code."
+        return 0
+    fi
+    
+    Get_urls ${suffix}
+    if [[ ${?} -ne 0 ]];then
+        echo -e "\033[1;31mERROR\033[0m : No images in this URL."
+        return 0
+    else
+        total_num=0
+        failed_num=0
+        success_num=0
+    fi
+    
+    for img_url in `echo "${urls}" | tr -s " " "\n"`
+    do
+    
+        # Get the image name. # 
+        img_name=$(echo "${img_url}" | grep -Eo '\S*\.\w\w\w' | xargs -I {} basename {})
+    
+        if [[ "x$(echo "${img_name}" | grep "${search_name}")" == "x" ]];then
+            continue;
+        fi
+    
+        # If there is no agreement in picture URL, add the agreement for hte URL's web address. # 
+        if [[ "x$(echo "${img_url}" | grep -Eo 'http\S*')" == "x" ]];then
+            img_url=$(echo "${img_url}" | sed 's/^\/*//g')
+
+            # Add the user specified missing path to picture URL. # 
+            if [[ "x${add_word}" != "x" ]];then
+                img_url="${source_protocol}//${add_word}${img_url}"
+            else 
+                img_url="${source_protocol}//${img_url}"
+            fi
+        fi
+    
+        # If there is no suffix in the picture name obtained from picture URL, add the ".jpg"  suffix to the name of the picture. # 
+        if [[ "x$(echo "${img_name}" | cut -d '.' -f 2)" == "x" ]];then
+            img_name="${img_name}.jpg"
+        fi
+    
+        # Skip repeating pictures. # 
+        Judge_img ${save_path} ${img_name}
+        if [[ ${?} -eq 0 ]];then
+            echo -e "\033[1;33mWARNING\033[0m : The image \"${img_name}\" already exists"
+            ((total_num++))
+            ((failed_num++))
+            continue
+        elif [[ ${?} -eq 1 ]];then
+            rm -rf ${save_path}${img_name}
+        fi
+        
+        # Download image from ${img_url} # 
+        Down_img ${save_path} ${img_name} ${img_url}
+    
+        if [[ ${?} -eq 0 ]];then
+            ((success_num++))
+            echo -e "\033[1;32m${img_url:0:30}... -> ${save_path:0:25}.../${img_name}\033[0m"
+        else
+            ((failed_num++))
+            echo -e "\033[31;mERROR\033[0m : The image \"${path_name:(-30)}\" download failed."
+        fi
+    
+        ((total_num++))
+    done
+    
+    wait
+    
+    # Print statistics. # 
+    echo -e "\033[1;32mTotal : ${total_num} \033[1;32mSuccess : ${success_num} \033[1;31mFailure : ${failed_num}\033[0m"
+    
+    # delete the file of save web source code, if the file in now path. # 
+    if [[ -f ./.re_pic.get.webcode ]];then
+        rm -rf ./.re_pic.get.webcode
+    fi
+    
+    return 0
+}
+
+main $@ 
 wait
 
-# print the end information.
-echo -e "\033[1;38m$(date +"%Y-%m-%d %H:%M:%S") Done.\033[0m"
-
-# print download total.
-echo -e "\033[1;33mTotal  : \033[1;34m${img_nums}\033[0m"
-
-# if hava the file of save web source code in now path, delete the file.
-if [[ -f ./.re_pic.get.webcode ]];then
-    rm -rf ./.re_pic.get.webcode
-fi
-
+# Print the end information. # 
+echo -e "\033[1;38mDone.\033[0m"
 exit 0
-
-
