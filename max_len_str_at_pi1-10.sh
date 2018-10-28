a02330bc3d13a85105cbbf79e9f6c9063c431b2a
sunowsir <sunowsir@protonmail.com>
2018-10-28 15:25:58 +0800

    update some file

--- max_len_str_at_pi1-10.sh
+++ max_len_str_at_pi1-10.sh
@@ -6,43 +6,55 @@
 #	Created Time: 2018年10月14日 星期日 16时47分44秒
 
 pi_cmd="
-grep -EIHron '[a-zA-Z]+' './' 2> /dev/null | awk -F ':' '
+grep -EIHron '[a-zA-Z0-9]+' './' 2> /dev/null | awk -F ':' -v hname=\$(hostname) '
 BEGIN{
     first = \$0; 
     second = \$0; 
     third = \$0;
-    first_len = length(first); 
-    second_len = length(second);
-    third_len = length(third);
+    first_len = length(\$3); 
+    second_len = length(\$3);
+    third_len = length(\$3);
 }
 {
     now_str = \$0;
-    now_length = length(\$3);
-    if (now_length > first_len) {
-        third = second; 
-        second = first; 
-        first = now_str; 
-        first_len = now_length; 
-    } else if (now_length > second_len && now_length < first_len) {
+    now_len = length(\$3);
+    if (now_len > first_len) {
         third = second;
+        third_len = second_len;
+        second = first;
+        second_len = first_len;
+        first = now_str; 
+        first_len = now_len; 
+    } else if (now_len > second_len) {
+        third = second;
+        third_len = second_len;
         second = now_str;
-        second_len = now_length;
-    } else if (now_length > third_len && now_length < second_len) {
+        second_len = now_len;
+    } else if (now_len > third_len) {
         third = now_str;
-        third_len = now_length;
+        third_len = now_len;
     }
 }
 END{
-    printf(\" first : \\n %s \\n second : \\n %s \\n third : \\n %s \\n\", first, second, third);
+    printf(\"%s:%d:%s\\n%s:%d:%s\\n%s:%d:%s\\n\", hname, first_len, first, hname, second_len, second, hname, third_len, third);
 }'"
 
-for i in `seq 1 10`;
-do
 
-    echo -e "\033[1;31mpi${i} : \033[0m"
-    echo -e "\033[1;32m"
-    ssh -t pi${i} ${pi_cmd}
-    echo -e "\033[0m"
+for i in `seq 1 10`; 
+do 
+    info="$(ssh -t pi${i} ${pi_cmd})"
+    if [[ $(echo "${info}" | head -1 | cut -d ':' -f2) -gt $(echo "${first}" | cut -d ':' -f2) ]];then
+        first=$(echo "${info}" | head -1)
+    fi
 
-done
+    if [[ $(echo "${info}" | head -2 | tail -1 | cut -d ':' -f2) -gt $(echo "${second}" | cut -d ':' -f2) ]];then
+        second=$(echo "${info}" | head -2 | tail -1)
+    fi
 
+    if [[ $(echo "${info}" | tail -1 | cut -d ':' -f2) -gt $(echo "${third}" | cut -d ':' -f2) ]];then
+        third=$(echo "${info}" | tail -1)
+    fi
+
+done 
+
+echo -e "${first}\n${second}\n${third}"
