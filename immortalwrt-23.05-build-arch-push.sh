7d15a71db63cc30b7a178a72740618431637717c
sunowsir <sunowsir@gmail.com>
2024-09-06 17:34:48 +0800

    [add] add build docker image sh

---
+++ immortalwrt-23.05-build-arch-push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#	* File     : immortalwrt-23.05-build-arch-push.sh
+#	* Author   : sunowsir
+#	* Mail     : sunowsir@163.com
+#	* Github   : github.com/sunowsir
+#	* Creation : 2024年08月15日 星期四 22时13分51秒
+
+set -x
+
+sudo podman login docker.io
+
+sudo podman rmi docker.io/sunowsir/immortalwrt-23.05-build:latest
+sudo podman rmi localhost/immortalwrt-23.05-build
+
+sudo podman commit 1dad52d9c598 immortalwrt-23.05-build
+sudo podman tag immortalwrt-23.05-build docker.io/sunowsir/immortalwrt-23.05-build:latest
+
+sudo podman push docker.io/sunowsir/immortalwrt-23.05-build:latest
+
+
+set +x
